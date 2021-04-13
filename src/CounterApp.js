4332dd9481d0f0227bf576a78dcda949baccf89e
Martin Obermeier <martin.obermeier@gmail.com>
2021-04-12 22:02:12 -0300

    Primer commit

---
+++ src/CounterApp.js
@@ -0,0 +1,44 @@
+// rafcp para crear el componente automaticamente
+// Manejo de eventos https://es.reactjs.org/docs/events.html
+// Los hooks son funciones
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types'
+
+const CounterApp = ( { value = 10 }) => {
+
+    // Hook
+    const [ counter, setCounter ] = useState( value );
+
+    const handleAdd = (e) => {
+        //console.log(e);
+        setCounter( counter + 1 );
+        //setCounter( (c) => c + 1 );
+    }
+    
+    const handleReset = () => {
+        setCounter( value );
+    }
+
+    const handleSubstract = () => {
+        setCounter( counter - 1 );
+    }
+
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2> { counter } </h2>
+
+            {/* <button onClick={ (e) => handleAdd(e) }>+1</button> */}
+            <button onClick={ handleAdd }>+1</button>
+            <button onClick={ handleReset }>Reset</button>
+            <button onClick={ handleSubstract }>-1</button>
+        </>
+    );
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number
+}
+
+export default CounterApp;
---
+++ src/PrimeraApp.js
@@ -0,0 +1,42 @@
+// import React, { Fragment } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types'
+
+// Functional Components
+// <Fragment></Fragment> para enviar mas elementos htmls sin incluirlos en un div extra
+// <></> fragment de React sin importar
+
+//const PrimeraApp = ( props ) => {
+//const PrimeraApp = ( { saludo = "Holis" } ) => {
+//const PrimeraApp = ( { saludo, subtitulo = "Soy un subtitulo" } ) => {
+const PrimeraApp = ( { saludo, subtitulo } ) => {
+    
+    //const saludo = 'Hola Mundo';
+    //const saludo = 1234;
+    //const saludo = true; // no funciona
+    //const saludo = {
+    //     nombre: 'Martin',
+    //     edad: 34
+    // };
+
+    return (
+        <>
+            <h1> { saludo } </h1>
+            {/* <pre>{ JSON.stringify( saludo, null, 3 ) }</pre> */}
+            <p> { subtitulo } </p>
+        </>
+    );
+
+}
+
+// Validar las propiedades
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+
+// Valores por defecto de las propiedades
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
+}
+
+export default PrimeraApp;
