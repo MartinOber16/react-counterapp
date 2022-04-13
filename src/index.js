bb7c13a05ba6132aea286d3eb89927def8ed5d48
Martin Obermeier <martin.obermeier@gmail.com>
2022-04-12 23:39:37 -0300

    Se actualizo a React 18

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+// Antes
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// //import PrimeraApp from './PrimeraApp';
+// import CounterApp from './CounterApp';
+// import './index.css';
+
+// const divRoot = document.querySelector('#app');
+ 
+// //ReactDOM.render( <PrimeraApp /> , divRoot );
+// //ReactDOM.render( <PrimeraApp saludo="Hola Mundo!" /> , divRoot );
+// //ReactDOM.render( <PrimeraApp saludo={123} /> , divRoot );
+// ReactDOM.render( <CounterApp value={ 10 } /> , divRoot );
+
+// React +18
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 //import PrimeraApp from './PrimeraApp';
 import CounterApp from './CounterApp';
 
+import './index.css';
+
 const divRoot = document.querySelector('#app');
- 
-//ReactDOM.render( <PrimeraApp /> , divRoot );
-//ReactDOM.render( <PrimeraApp saludo="Hola Mundo!" /> , divRoot );
-//ReactDOM.render( <PrimeraApp saludo={123} /> , divRoot );
-ReactDOM.render( <CounterApp value={ 100 } /> , divRoot );
+
+const root = createRoot(divRoot);
+root.render( <CounterApp value={ 10 } /> );
